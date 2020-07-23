8846847225950285b5237468b726db749d30d358
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-07-23 12:51:33 +0200

    generic types

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,20 @@
 "use strict";
-class Person {
-    constructor(n) {
-        this.name = n;
-    }
-    greet(phrase) {
-        console.log(phrase + " " + this.name);
-    }
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-let user;
-user = new Person("FEE");
-user.greet("Bonjour les amis: ");
+const mergedObj = merge({ name: 'Ibr', hobbies: ['sports'] }, { age: 44 });
+mergedObj.age = 33;
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements.`;
+    }
+    return [element, descriptionText];
+}
+function extractAndConvert(obj, key) {
+    return 'Value: ' + obj[key];
+}
+console.log(extractAndConvert({ name: "Ibrah", age: 45 }, "age"));
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,46 @@
+// const names: Array<string> = [];
 
+// const promise: Promise<string> = new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         resolve('This is done! ')
+//     }, 2000);
+// });
+// promise
+// .then(data=>{
+//     console.log("data herer", data);
 
-interface Named {
-    readonly name?: string;
-    outputName?: string;
+// }) 
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
 }
-interface Greetable extends Named {
-    greet(phrase: string): void
+// const mergedObj = merge({ name: 'Ibr' }, { age: 44 }) as {name: string, age: number};
+const mergedObj = merge({ name: 'Ibr', hobbies: ['sports'] }, { age: 44 });
+mergedObj.age = 33
+
+//###
+interface Lengthy {
+    length: number | string;
 }
-
-class Person implements Greetable {
-
-    name?: string;
-    age: 42;
-
-    constructor(n?: string) {
-        this.name = n;
+function countAndDescribe<S extends Lengthy>(element: S): [S, string] {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    } else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements.`;
     }
-    outputName?: string;
-    greet(phrase: string) {
-        console.log(phrase + " " + this.name);
-    }
 
+    return [element, descriptionText];
 }
-let user: Greetable;
 
-user = new Person("FEE");
-user.greet("Bonjour les amis: ");
+// console.log(countAndDescribe("Hi, we are the world"));
+// console.log(countAndDescribe(["Hi, we are the world", "ARR"]));
+// console.log(countAndDescribe([{ name: "Hi, we " }, { name: "Hi, we " }, { name: "FR" }]));
 
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+
+    return 'Value: ' + obj[key];
+}
+
+console.log(extractAndConvert({name: "Ibrah", age:45}, "age"));
