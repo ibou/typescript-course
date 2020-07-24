41bd69c40f5e6372329447df0a905e540d13c02c
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-07-24 03:22:46 +0200

    pr1

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,61 @@
-
-
-interface Named {
-    readonly name?: string;
-    outputName?: string;
+// autobind decorator
+function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
 }
-interface Greetable extends Named {
-    greet(phrase: string): void
-}
 
-class Person implements Greetable {
+class ProjectInput {
 
-    name?: string;
-    age: 42;
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
-    constructor(n?: string) {
-        this.name = n;
+    constructor() {
+        this.templateElement = document.getElementById(
+            'project-input'
+        )! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.configure();
+        this.attach();
     }
-    outputName?: string;
-    greet(phrase: string) {
-        console.log(phrase + " " + this.name);
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log("Submitted ...", this.titleInputElement.value);
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+
     }
 
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
 }
-let user: Greetable;
 
-user = new Person("FEE");
-user.greet("Bonjour les amis: ");
-
+const prjInput = new ProjectInput();
